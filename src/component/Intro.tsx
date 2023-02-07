6352f5da4b961abc4c560e5c4d44af8a60601bfc
danGbe <73373221+danidji@users.noreply.github.com>
2023-02-07 23:42:24 +0100

    style : add styles

--- src/component/Intro.tsx
+++ src/component/Intro.tsx
@@ -1,31 +1,52 @@
 import Image from "next/image";
-import profilePicture from "../../public/clelya-portrait-medium.jpeg";
+import profilePicture from "../../public/clelya-portrait-medium.png";
 import cabinetPicture from "../../public/cabinet-medium.jpeg";
+import styles from "@/styles/Intro.module.css";
 
 export default function Intro() {
     return (
-        <div className="IntroWrapper">
-            <h1>Cabinet Clelya Eva</h1>
-            <p>Medium Voyance Cartomanice</p>
-            <p>Vous souhaitez connaître votre futur ou communiquer avec le monde invisible.</p>
-            <p>Bienvenue sur mon site.</p>
-            <div className="PresentationContainer">
-                <div className="PresentationContainer1">
-                    <Image src={profilePicture} alt="Photo Medium Clelya" />
+        <div className={styles.intro_wrapper}>
+            <div className={styles.intro_title}>
+                <h1>Cabinet Clelya Eva</h1>
+                <p className={styles.subtitle}>Medium - Voyance - Cartomancie</p>
+            </div>
+            <p className={styles.subtitle}>Vous souhaitez connaître votre futur ou communiquer avec le monde invisible.</p>
+            <p className={styles.subtitle}>Bienvenue sur mon site.</p>
+            <div className={styles.presentation_container}>
+                <div className={styles.presentation_container1}>
+                    <div className={styles.image_container}>
+                        <Image
+                            src={profilePicture}
+                            alt="Photo Medium Clelya"
+                            fill
+                            style={{
+                                objectFit: "cover",
+                            }}
+                        />
+                    </div>
                     <p>
                         Bonjour je suis Clelya médium et voyante expérimentée basée à Toul dans le 54. Je suis heureuse de pouvoir offrir mes services
                         à des clients du monde entier.
                     </p>
                 </div>
-                <div className="PresentationContainer2">
-                    <Image src={cabinetPicture} alt="Photo Cabinet Medium Toule" />
+                <div className={styles.presentation_container2}>
+                    <div className={styles.image_container}>
+                        <Image
+                            src={cabinetPicture}
+                            alt="Photo Cabinet Medium Toule"
+                            fill
+                            style={{
+                                objectFit: "cover",
+                            }}
+                        />
+                    </div>
                     <p>
                         Je vous accueille du lundi au samedi pour des consultations sur rendez-vous dans mon cabinet situé à Toul également en visio
                         sur Wathsapp
                     </p>
                 </div>
             </div>
-            <div className="InfoCitation">Je suis impatiente de vous aider à traverser les épreuves de la vie et à découvrir votre futur.</div>
+            <p className={styles.info_citation}>Je suis impatiente de vous aider à traverser les épreuves de la vie et à découvrir votre futur.</p>
         </div>
     );
 }
--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,3 +1,5 @@
+import styles from "@/styles/Home.module.css";
+
 const nav_menu = [
     {id: "story", label: "Mon histoire"},
     {id: "method", label: "Mes méthodes"},
@@ -6,11 +8,13 @@ const nav_menu = [
 
 export default function NavBar() {
     return (
-        <div className="NavBarWrapper">
+        <div className={styles.navbar_wrapper}>
             <ul>
                 {nav_menu.map((item, id) => (
                     <li key={`nav-menu-${id}`}>
-                        <a href={`#${item.id}`}>{item.label}</a>
+                        <a href={`#${item.id}`}>
+                            <h4>{item.label}</h4>
+                        </a>
                     </li>
                 ))}
             </ul>
