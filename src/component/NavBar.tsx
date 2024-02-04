48d91f772015d9d55d23f68a8f313d668118eeea
danGbe <73373221+danidji@users.noreply.github.com>
2024-02-04 21:48:09 +0100

    feat : change infos

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,10 +1,11 @@
 import styles from "@/styles/Home.module.css";
+import navStyles from "@/styles/NavBar.module.css";
 
 const nav_menu = [
-    {id: "story", label: "Mon histoire"},
-    {id: "method", label: "Mes méthodes"},
-    {id: "session", label: "Mes séances"},
-    {id: "boutique", label: "Boutique"},
+    {id: "story", label: "Mon Histoire"},
+    {id: "method", label: "Mes Méthodes"},
+    {id: "session", label: "Mes Séances"},
+    {id: "infos", label: "Infos"},
 ];
 
 export default function NavBar({closeMenu}: {closeMenu?: () => void}) {
@@ -16,7 +17,7 @@ export default function NavBar({closeMenu}: {closeMenu?: () => void}) {
             <ul>
                 {nav_menu.map((item, id) => (
                     <li key={`nav-menu-${id}`}>
-                        <a href={item.id === "boutique" ? `/${item.id}` : `/#${item.id}`} onClick={() => onClose()}>
+                        <a href={item.id === "infos" ? `/${item.id}` : `/#${item.id}`} onClick={() => onClose()} className={navStyles.text}>
                             <h4>{item.label}</h4>
                         </a>
                     </li>
--- src/component/Session.tsx
+++ src/component/Session.tsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/Session.module.css";
 import Link from "next/link";
-import {Price} from "@/pages/boutique";
+import {Price} from "@/pages/infos";
+import SessionDetail from "./SessionDetail";
 
 export default function Session() {
     return (
@@ -9,8 +10,12 @@ export default function Session() {
             <h2>Mes séances</h2>
             <div className={styles.session_content}>
                 <h3>Tarifs</h3>
-                <p className={styles.prices}>30 min / {Price["30MIN"]}€</p>
-                <p className={styles.prices}>60 min / {Price["60MIN"]}€</p>
+                <p className={styles.prices_title}>Au cabinet</p>
+                <p className={styles.prices}>{Price["60MIN"]}€/h</p>
+                <p className={styles.prices_title}>En visio</p>
+                <p className={styles.prices}>8min - 18,90€</p>
+                <p className={styles.prices}>15min - 29,90€</p>
+                <p className={styles.prices}>30min - 57,90€</p>
                 <h3>Déroulement d&lsquo;une séance</h3>
                 <p>
                     Les séances peuvent se faire à mon cabinet situé à Toul (54) au :<br />
@@ -23,7 +28,7 @@ export default function Session() {
                     Prendre Rendez-Vous : <strong>07 63 75 72 73</strong>
                 </p>
                 <p>
-                    <Link href={"/boutique"}>Réserver une séance</Link>
+                    <Link href={"/boutique"}>Voir plus de détail sur les séances</Link>
                 </p>
             </div>
         </div>
